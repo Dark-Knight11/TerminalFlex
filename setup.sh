91fb95dac85255b32f8c36ac7415b890e3ee4820
HyperNova <hypernova114@gmail.com>
2020-10-24 14:34:59 +0530

    added the command to create text files in the code

--- setup.sh
+++ setup.sh
@@ -3,6 +3,8 @@ sudo apt update
 sudo apt install figlet
 sudo apt install lolcat
 username=$(whoami)
+sudo touch banner.txt text.txt
+sudo chmod 666 banner.txt text.txt
 echo $username > text.txt
 cat text.txt | figlet > banner.txt
 echo -e "\ncat /opt/TerminalFlex/banner.txt | lolcat" >> /home/$username/.bashrc
